072a587f70bf5975f2708f0170b83cda2bcdce9c
Amelia Scott <amelia.scott@roh.org.uk>
2025-01-09 20:39:43 +0000

    Fix critical repeat generation vulnerability

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,73 @@ import Result from './components/Result';
 import { adjectives } from './words/adjectives'
 import { nouns } from './words/nouns'
 
-function App() {
+const REGENERATION_INTERVAL_IN_MS = 1000 * 60 * 60 * 24 * 12 // 12 Days
 
+function App() {
   const [adjectivesList, setAdjectivesList] = useState<string[]>([]);
   const [nounsList, setNounsList] = useState<string[]>([]);
-  const [sprintName, setSprintName] = useState<string>("");
+  const [sprintName, setSprintName] = useState<string | undefined>(undefined);
+  const [shouldGenerate, setShouldGenerate] = useState<boolean>(false);
+  const [generateButtonText, setGenerateButtonText] = useState<'Generate' | 'Regenerate'>('Generate');
+  const [lastGenerated, setLastGenerated] = useState<string | undefined>(undefined);
+
+  const intervalElapsed = (lastGenerated: string): boolean => (
+    (Date.now() - (+lastGenerated)) > REGENERATION_INTERVAL_IN_MS
+  )
 
   useEffect(() => {
+    const lastGeneratedCookieValue = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("lastSprintNameGeneration="))
+      ?.split("=")[1];
+
+    const sprintNameCookieValue = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("currentSprintName="))
+      ?.split("=")[1];
+
+    setLastGenerated(lastGeneratedCookieValue);
     setAdjectivesList(adjectives);
     setNounsList(nouns);
-  }, [])
+    setSprintName(sprintNameCookieValue);
+    setGenerateButtonText(sprintName ? 'Regenerate' : 'Generate');
+    setShouldGenerate(!lastGenerated || !sprintName || intervalElapsed(lastGenerated))
+  }, [lastGenerated, sprintName])
 
   const handleGenerate = () => {
-    const selectedAdjIndex = Math.floor(Math.random() * adjectivesList.length)
-    const selectedNounIndex = Math.floor(Math.random() * nounsList.length)
+    const selectedAdjIndex = Math.floor(Math.random() * adjectivesList.length);
+    const selectedNounIndex = Math.floor(Math.random() * nounsList.length);
+    const now = String(Date.now());
+    const newSprintName = `${adjectivesList[selectedAdjIndex]} ${nounsList[selectedNounIndex]}`
+    const newLastGeneratedCookie = `lastSprintNameGeneration=${now}`;
+    const newCurrentSprintNameCookie = `currentSprintName=${newSprintName}`;
 
-    setSprintName(`${adjectivesList[selectedAdjIndex]} ${nounsList[selectedNounIndex]}`)
-}
+    document.cookie = newLastGeneratedCookie;
+    document.cookie = newCurrentSprintNameCookie;
+
+    setSprintName(newSprintName);
+    setLastGenerated(now);
+    setGenerateButtonText('Regenerate');
+    setShouldGenerate(false);
+  }
 
 
   return (
     <div className="App">
-      Siegfried Sprint Name Generator
+      <h3>Siegfried Sprint Name Generator</h3>
+      {sprintName && (
+        <>
+          <>Siegfried's current sprint name is...</>
+          <Result text={sprintName}/>
+        </>
+      )}
       <div>
-        <button onClick={handleGenerate}>Generate</button>
+        {shouldGenerate && (
+          <div>
+            <button onClick={handleGenerate}>{generateButtonText}</button>
+          </div>
+        )}
       </div>
-      <Result text={sprintName}/>
     </div>
   );
 }
